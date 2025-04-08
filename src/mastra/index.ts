7138d5c2d1d5a78fe6384d520f5da1bab8e51025
D-lyw <liuyuanwang4321@gmail.com>
2025-04-08 17:24:48 +0800

    feat: successfully set mastra agents service deploy config

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,7 +1,5 @@
-import { CloudflareDeployer } from '@mastra/deployer-cloudflare';
 import { Mastra } from '@mastra/core';
 import { createLogger } from '@mastra/core/logger';
-
 import { tappingAgent } from './agents/index';
 // import { xAgent } from './agents/xAgent.js';
 import { ckbDocAgent } from './agents/ckbDocAgent';
@@ -21,21 +19,6 @@ const pgVector = new PgVector(process.env.POSTGRES_CONNECTION_STRING || '');
 
 // 使用最简单的配置
 export const mastra = new Mastra({
-  // deployer: new CloudflareDeployer({
-  //   scope: process.env.CF_ACCOUNT_ID,
-  //   projectName: 'tapping-agent',
-  //   auth: {
-  //     apiToken: process.env.CF_API_TOKEN,
-  //     apiEmail: process.env.CF_API_EMAIL,
-  //   },
-  //   routes: [
-  //     {
-  //       pattern: "agents.nervepuppy.xyz",
-  //       zone_name: "nervepuppy.xyz",
-  //       custom_domain: true,
-  //     }
-  //   ]
-  // }),
   workflows: {
     ckbWorkflow,
     nostrContentTappingWorkflow
